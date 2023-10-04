1dd5ed685f6c718684f73c165559d840fdd191fd
matheusrodrigues1 <matech00@outlook.com>
2023-10-04 15:15:16 -0300

    config do projeto, criação do Header e inicio da pagina de Cadastro

---
+++ app/Components/Cadastrar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Cadastrar() {
+  return (
+    <>
+      <div className='ml-72 h-screen w-full bg-slate-500'>
+        <h1>Hello next</h1>
+      </div>
+    </>
+  )
+}
+
+export default Cadastrar
---
+++ app/Components/Header.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='flex flex-col absolute bg-slate-950 h-screen w-72 pl-8 pt-10 text-white gap-28'>
+      <h3>Hello</h3>
+      <div className='flex flex-col gap-2'>
+        <button className='h-10 w-56 bg-red-600 rounded'>
+          <Link href="/">Visualizar prestadores</Link>
+        </button>
+        <button className='h-10 w-56 bg-red-600 rounded'>
+          <Link href="/">Cadastrar prestador</Link>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
