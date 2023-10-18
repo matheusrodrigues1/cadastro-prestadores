02253da878da1c80cdb84210daec88a54919d3b0
matheusrodrigues1 <matech00@outlook.com>
2023-10-18 19:06:51 -0300

    responsividade

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -6,16 +6,16 @@ import Icone from '../images/loop.svg';
 function Header() {
   return (
     <>
-      <div className='flex flex-col items-center pt-2 bg-slate-950 h-screen w-80 text-white gap-28'>
-        <Image src={Icone} alt='icon' className='h-40 w-40'/>
-        <div className='flex flex-col gap-4'>
-          <button className='h-10 w-56 bg-red-600 rounded-xl'>
+      <div className='flex flex-col border-transparent items-center bg-slate-950 h-screen w-80 text-white gap-28 lg:gap-14 md:gap-10'>
+        <Image src={Icone} alt='icon' className='h-36 w-36 lg:h-24 lg:w-24 md:h-24'/>
+        <div className='flex flex-col gap-4 lg:gap-2 md:gap-1'>
+          <button className='p-2 whitespace-nowrap  bg-red-600 rounded-xl'>
             <Link href="/visualizacao">Visualizar prestadores</Link>
           </button>
-          <button className='h-10 w-56 bg-red-600 rounded-xl'>
+          <button className='p-2 whitespace-nowrap bg-red-600 rounded-xl'>
             <Link href="/">Cadastrar prestador</Link>
           </button>
-          <button className='h-10 w-56 bg-red-600 rounded-xl'>
+          <button className='p-2 whitespace-nowrap bg-red-600 rounded-xl'>
             <Link href="/import">Importar prestador</Link>
           </button>
         </div>
